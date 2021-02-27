cef2b4827c680df237376327b860ac3e4aed966d
Micheal Erickson <micheal.erickson3.14@gmail.com>
2021-02-27 10:20:33 -0700

    gh-pages

---
+++ precache-manifest.b19296a2480d14aee81b4a9d411e8e4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2597f6fa7ef1724d0cd",
+    "url": "/css/app.bd70d789.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dc1c58b83886b2580f59cb927889e9ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6902d301f52aff66b6d9",
+    "url": "/js/about.bdbcc647.js"
+  },
+  {
+    "revision": "a2597f6fa7ef1724d0cd",
+    "url": "/js/app.5f8244b8.js"
+  },
+  {
+    "revision": "62bb3203c9a1b4b0658d",
+    "url": "/js/chunk-vendors.0e055c2d.js"
+  },
+  {
+    "revision": "442c94ebe800f3161ff9cfaf9add627d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
