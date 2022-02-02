40d3abf0ca60cfe469fd7dd5ead08a76f2e904dd
Micheal Erickson <micheal.erickson3.14@gmail.com>
2022-02-02 00:27:51 -0700

    report build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,24 +11,24 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js")
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
   "/precache-manifest.c99ab1ba212ad52c89bb86698b6f2991.js"
-)
+);
 
-workbox.core.setCacheNameDetails({prefix: "mikese314.github.io"})
+workbox.core.setCacheNameDetails({prefix: "mikese314.github.io"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting()
+    self.skipWaiting();
   }
-})
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
